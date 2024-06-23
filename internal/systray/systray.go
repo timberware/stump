99d4ece449b0fbe78a0b16ba9e3ab4ebd64a24a0
Josue <manekenpix@fastmail.com>
2024-06-23 15:56:41 -0400

    feat: add websocket connection

--- internal/systray/systray.go
+++ internal/systray/systray.go
@@ -1,19 +1,57 @@
 package systray
 
 import (
-	"github.com/getlantern/systray"
 	"stump/internal/icon"
 	"stump/internal/logger"
+	"stump/internal/ws"
+
+	"github.com/getlantern/systray"
 )
 
+var connect *systray.MenuItem
+var disconnect *systray.MenuItem
+var token *systray.MenuItem
+var quit *systray.MenuItem
+var m chan string
+
 func OnReady() {
-	logger.Info("Application ready")
 	systray.SetTitle("Stump")
 	systray.SetIcon(icon.Data())
 
-	SetupMenu()
+	connect = systray.AddMenuItem("Connect", "Connect to Twitch")
+	disconnect = systray.AddMenuItem("Disconnect", "Disconnect from Twitch")
+	token = systray.AddMenuItem("Token", "Twitch Token")
+	quit = systray.AddMenuItem("Quit", "Quit the app")
+
+	go Handle()
+
+}
+
+func Handle() {
+	m = make(chan string)
+	for {
+		select {
+		case <-connect.ClickedCh:
+			logger.Info("Connecting")
+			ws.Connect(m)
+		case <-disconnect.ClickedCh:
+			logger.Info("Disconnecting")
+			ws.Disconnect()
+		case <-token.ClickedCh:
+			GetTwitchToken("Please input your Twitch Token", "Get Token")
+		case <-quit.ClickedCh:
+			shouldQuit, _ := ConfirmQuit("Do you want to continue?", "Confirm Quit?")
+
+			if shouldQuit {
+				systray.Quit()
+			}
+		case message := <-m:
+			logger.Info(message)
+		}
+	}
+
 }
 
 func OnExit() {
-	logger.Info("Application closed")
+	logger.Info("Bye Bye")
 }
--- internal/systray/ui.go
+++ internal/systray/ui.go
@@ -2,13 +2,11 @@ package systray
 
 import (
 	"github.com/ncruces/zenity"
-	"stump/internal/logger"
 )
 
 func GetTwitchToken(text, title string) (string, error) {
 	result, err := zenity.Entry(text, zenity.Title(title))
 	if err != nil {
-		logger.Error("Failed to open input dialog", "error", err)
 		return "", err
 	}
 
